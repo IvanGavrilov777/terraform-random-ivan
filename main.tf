75fa7460881f4f419fa4aaa05b0d9938db8009b2
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2026-08-25 15:15:30 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,12 @@ resource "tfe_team" "goo" {
     name = "new"
     organization = "petya-business-org"
 }
+
+resource "tfe_project" "test" {
+  organization = "petya-business-org"
+  name         = var.tfe_project_name
+}
+
+variable "tfe_project_name" {
+default = "peci-default"
+}
